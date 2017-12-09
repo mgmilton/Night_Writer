eb0b842cb1baccde7a9f3ea10dad9132edba6802
Ellen Cornelius <ecorneli@iwu.edu>
2017-12-09 11:57:11 -0700

    initial commit

---
+++ test/night_writer_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_writer'
+
+class NightWriterTest < Minitest::Test
+
+def test_if_it_works
+  nightwriter = NightWriter.new
+
+end
+
+
+end
